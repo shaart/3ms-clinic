6dfde3aaf89f317a5fce6ecc49c7d2f07409871c
Artur Shalaev <imshaart@gmail.com>
2019-04-20 16:44:14 +0300

    [CONFIG] Cleaned build.gradle and added discovery for tms-config, modified tms-core build.gradle and added HelloDto and HelloClient

---
+++ tms-core/src/main/java/com/tms/platform/common/dto/HelloDto.java
@@ -0,0 +1,14 @@
+package com.tms.platform.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloDto {
+
+  int id;
+  String name;
+}
